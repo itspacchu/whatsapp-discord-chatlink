691f60dc56d68b46615ef97e46f06442638fc4de
Pacchu <37984032+itspacchu@users.noreply.github.com>
2020-05-12 20:22:02 +0530

    added linkers to whatsapp messages

--- discord_test.py
+++ discord_test.py
@@ -1,20 +1,30 @@
-import sys , os , discord , subprocess , json , io , random , COVID19Py ,time ,numpy,glob
 
+'''
+more simplified chan for better use and updatability
+'''
 
+import sys , os , discord , subprocess , json , io , random , COVID19Py ,time ,numpy,glob,asyncio
+
+botid = "eheheh no :D pls"
+
+#file variables
 f = io.open("call.json", mode="r", encoding="utf-8")
-rep_pkg = json.load(f)
-spc = " "
+filesend = None
+
+
+#reply handling variables
 flag = False
 secondary_flag = False
+
+rep_pkg = json.load(f)
+
+#uselessly useful variables
+spc = " "
 cv19 = COVID19Py.COVID19()
-ispaused = False
-randkeygen = random.randint(00000000,99999999)
-termination_check = False
-main_hashkeyfunction = 1337
-attempt = 0
-terminated_mode = False
-filesend = None
-wished_users_list = []
+client = discord.Client()
+oldfile = []
+
+
 
 def system_call(command):
     p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
@@ -22,62 +32,54 @@ def system_call(command):
     p_status = p.wait()
     return (output,err)
 
-client = discord.Client()
-
 @client.event
 async def on_ready():
+    global oldfile
     print("Bot's Up and runnin bois")
     activity = discord.Game(name="Helping Pacchu Fail")
     await client.change_presence(status=discord.Status.online, activity=activity)
 
-
-#on message calling function
-
-#for ... in get_all_members()
 @client.event
 async def on_member_update(before, after):
-    global flag,wished_users_list
+    global flag,filesend,oldfile
+    channel = client.get_channel(709419678723866635)
+
+    try:
+        msgfile = open("whatsappmessages.txt","r")
+        msgs = msgfile.readlines()
+        if(len(msgs) > 2):
+            for k in msgs:
+                await channel.send("[WA] >> "+k)
+                time.sleep(4)
+            msgs = []
+            system_call("del whatsappmessages.txt")
+        else:
+            print("might be spam .. ignoring")
+    except FileNotFoundError:
+        print("404 no wa messages file so ignorein")
+
     if(str(after.status) == "online" and str(before.status) == "offline"): 
         for _ in client.get_all_channels():
                 if("bot" in _.name and not ("pacchu" in after.name)):
                     await _.send( random.choice(rep_pkg["convos"]["person_online"])  + spc + after.mention)
 
 
-            
+
+
+
 @client.event
 async def on_message(message):
-    global flag,secondary_flag,ispaused,randkeygen,attempt,termination_check,terminated_mode,filesend,wished_users_list
-    if(ispaused):
-        time.sleep(120)
-        ispaused = False
-    if message.author == client.user:
-        flag = True
-        return
-    
-    if(message.content.startswith("!absolute_termination")):
-        termination_check = True
-        await message.channel.send("Initiating Absolute Termination override\nEnter the corresponding key for {}".format(randkeygen))
-    
-    if(termination_check and (attempt < 5) and (not terminated_mode)):
-        if(int(message.content) == int(randkeygen/main_hashkeyfunction + randkeygen*main_hashkeyfunction)):
-            await message.channel.send("Hash key matched\nTerminating ...")
-        else:
-            randkeygen = random.randint(00000000,99999999)
-            await message.channel.send("Incorrect hash key ...  try with a new hashkey : {} \n Attempt : {}/4".format(randkeygen,attempt))
-            attempt+= 1
-        attempt = 0
-        termination_check = False
+    #important Transfer variables
+    global flag,filesend
 
-    if(not terminated_mode):
+
+    to_reply = important_functions(message)
+
+
+    #handle conversion after important duties
+
+    if(to_reply == None):
         to_reply = single_starter_replies(message)
-    else:
-        to_reply = None
-
-    if(message.content == "!absolute_start" and terminated_mode):
-        attempt = 0
-        terminated_mode = False
-        termination_check = True
-        await message.channel.send(random.choice(rep_pkg["convos"]["reanimated"]))
 
 
     try:
@@ -92,26 +94,8 @@ async def on_message(message):
     
 
 
-
-def single_starter_replies(message):
-    global flag,cv19,ispaused,filesend
-    if(message.content.startswith("!cmd")):
-        print("waiting for command")
-        if(len(message.content.lower()) > 4):
-            givencommand = str(message.content)[5:]
-            print(givencommand)
-            if(  (any(msg in givencommand for msg in rep_pkg["prohibited_commands"]) )):
-                return "pls" +spc+ random.choice(rep_pkg["convos"]["rejection"]) + spc + random.choice(rep_pkg["emojicons"]["broken"])
-            try:
-                toshow = system_call(givencommand)[0].decode()
-                return  x+(str(toshow)[:1800] )
-            except:
-                try:
-                    return system_call(script)
-                except:
-                    return "what did u send tho...? wut?? \n >> {}".format(givencommand)
-    
-    elif(message.content.startswith("!upload")):
+def important_functions(message):
+    if(message.content.startswith("!upload")):
         nameOfFile = str(message.content)[8:]
         print(nameOfFile)
         #files = os.listdir('./music/')
@@ -126,20 +110,17 @@ def single_starter_replies(message):
                 return "Uploading {}".format(str(name)) + spc + random.choice(rep_pkg["emojicons"]["broken"])
         return "404 whoopsie" + spc + random.choice(rep_pkg["emojicons"]["broken"])
 
-    elif(message.content.startswith("!remove")):
-        nameOfFile = str(message.content)[8:]
-        print(nameOfFile)
-        files = os.listdir('.')
-        print(files)
-        print((nameOfFile in list(files)))
-        for name in files:
-            print(str(name))
-            if(nameOfFile in str(name) and (name != "discord_test.py") and (name != "call.json")):
-                print(name)
-                system_call("rm {}".format(name))
-                return "Found file ... Removing"
-        return "404 whoopsie" + spc + random.choice(rep_pkg["emojicons"]["broken"])
 
+
+
+
+def single_starter_replies(message):
+    global flag,cv19,ispaused,filesend
+    if(message.channel == client.get_channel(709419678723866635)):
+        return None
+    if(message.content.startswith("!cmd")):
+        return "Pacchu has said not to use this complex shit anymore so.. yeah im sorry kids :D"
+    #Replies handler
 
     elif((any(msg in message.content.lower() for msg in rep_pkg["triggers"]["covidcases"])) and flag):
         loc_data = cv19.getLocationByCountryCode('In')[0]
@@ -205,8 +186,4 @@ def randomizer(prob):
     else:
         return False
 
-
-
-
-
-client.run("NzA2MzkyOTc1NTI1MTUwNzgw.Xq5mSw.Xi8nkVTTIT4QpNCa0wv2GW4OwiU")
+client.run(botid)
